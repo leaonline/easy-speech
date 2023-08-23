94e19a826bad9ede8486381b4a9fa7ab37edec4f
Piero Giusti <pierophp@gmail.com>
2023-08-23 11:41:18 -0300

    Fix babel

--- rollup.demo.config.js
+++ rollup.demo.config.js
@@ -1,4 +1,4 @@
-import babel from '@rollup/plugin-babel'
+import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from '@rollup/plugin-terser'
 
